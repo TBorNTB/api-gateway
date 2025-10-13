ea281fb764fc4418645f00d53548c45215a15c40
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2025-10-13 18:44:04 +0900

    chore: 헬스 체크

--- src/main/java/com/sejong/gateway/TestController.java
+++ src/main/java/com/sejong/gateway/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/")
+    @GetMapping("/actuator/health")
     public String health() {
         return "OK";
     }

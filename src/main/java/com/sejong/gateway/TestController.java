d7cc310c043fc61b413f4f776e2904ea1513e6d1
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2025-10-20 17:53:07 +0900

    test: 에러 로그 찍기

--- src/main/java/com/sejong/gateway/TestController.java
+++ src/main/java/com/sejong/gateway/TestController.java
@@ -1,8 +1,10 @@
 package com.sejong.gateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class TestController {
 
@@ -12,7 +14,7 @@ public class TestController {
     }
 
     @GetMapping("/error")
-    public String testLoggingError(){
-        throw new IllegalArgumentException();
+    public void testLoggingError(){
+        log.error("CloudWatch 전송 테스트 - {}", System.currentTimeMillis());
     }
 }
